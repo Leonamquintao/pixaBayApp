93729ab5de6ff7efc948c30b7929cf31dd402c9c
Leonam Quintão <leonamquintao@gmail.com>
2022-06-24 09:58:11 -0300

    add search header

---
+++ src/components/SearchHeader.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+interface SearchHeaderProps {
+  searchTerm: string;
+  clearInput: () => void;
+  onKeyPress: () => void;
+  onChangeText: (text: string) => void;
+}
+
+const SearchHeader: React.FC<SearchHeaderProps> = ({
+  searchTerm,
+  clearInput,
+  onKeyPress,
+  onChangeText,
+}) => {
+  const {
+    header,
+    headerRow,
+    inputContainer,
+    closeIcon,
+    inputStyle,
+    searchIconContainer,
+  } = styles;
+  return (
+    <View style={header}>
+      <View style={headerRow}>
+        <View style={inputContainer}>
+          <TextInput
+            style={inputStyle}
+            value={searchTerm}
+            placeholder={'search'}
+            autoCapitalize="none"
+            placeholderTextColor="#a55266"
+            onChangeText={onChangeText}
+            onKeyPress={onKeyPress}
+          />
+          {searchTerm && (
+            <TouchableOpacity style={closeIcon} onPress={clearInput}>
+              <Icon name="close" color="#6e5d96" size={14} />
+            </TouchableOpacity>
+          )}
+        </View>
+        <View style={searchIconContainer}>
+          {searchTerm.length > 3 ? (
+            <ActivityIndicator color={'#b64190'} />
+          ) : (
+            <Icon name="search" size={24} color={'#b64190'} />
+          )}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 0.2,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingTop: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: '#6e5d96',
+  },
+  headerRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  inputContainer: {flex: 1},
+  closeIcon: {
+    position: 'absolute',
+    width: 22,
+    height: 22,
+    backgroundColor: '#C0C0C0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    top: 18,
+    right: 12,
+  },
+  inputStyle: {
+    height: 54,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#1d72bd',
+    paddingVertical: 4,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  searchIconContainer: {
+    flex: 0.2,
+    borderWidth: 1,
+    borderColor: '#1d72bd',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 4,
+  },
+});
+
+export default SearchHeader;
--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,45 +1,26 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {View, Text, StyleSheet} from 'react-native';
+import SearchHeader from '../components/SearchHeader';
 
 const HomePage: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const {
-    container,
-    header,
-    headerRow,
-    inputContainer,
-    inputStyle,
-    searchIconContainer,
-    content,
-  } = styles;
+  const {container, content} = styles;
 
   const triggerSearch = () => {
     console.log(searchTerm);
   };
 
+  const clearInput = () => setSearchTerm('');
+
   return (
     <View style={container}>
-      <View style={header}>
-        <View style={headerRow}>
-          <View style={inputContainer}>
-            <TextInput
-              style={inputStyle}
-              value={searchTerm}
-              placeholder={'search'}
-              autoCapitalize="none"
-              placeholderTextColor="#a55266"
-              onChangeText={(text: string) => setSearchTerm(text)}
-              onKeyPress={triggerSearch}
-            />
-          </View>
-          <View style={searchIconContainer}>
-            <Icon name="ios" size={30} color={'#b64190'} />
-          </View>
-        </View>
-      </View>
-
+      <SearchHeader
+        searchTerm={searchTerm}
+        clearInput={clearInput}
+        onKeyPress={triggerSearch}
+        onChangeText={setSearchTerm}
+      />
       <View style={content}>
         <Text>Content Here... FlatList</Text>
       </View>
@@ -52,35 +33,10 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20,
   },
-  header: {
-    flex: 0.2,
-    justifyContent: 'center',
-    paddingHorizontal: 16,
-    paddingTop: 10,
-    borderBottomWidth: 0.5,
-    borderBottomColor: '#6e5d96',
+  content: {
+    flex: 1,
+    paddingVertical: 10,
   },
-  headerRow: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  inputContainer: {flex: 1},
-  inputStyle: {
-    height: 54,
-    fontSize: 16,
-    borderWidth: 1,
-    borderColor: '#1d72bd',
-    paddingVertical: 4,
-    paddingHorizontal: 10,
-    borderRadius: 5,
-  },
-  searchIconContainer: {
-    flex: 0.2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 4,
-  },
-  content: {flex: 1, paddingVertical: 10},
 });
 
 export default HomePage;
